939f8ac3b6a79776f7b668c85beee0090242c826
Goffayoh <yohann.goffart@etu.univ-rouen.fr>
2021-02-01 11:48:13 +0100

    Page de la toolbox avec router

---
+++ src/views/Toolbox.vue
@@ -0,0 +1,40 @@
+<template>
+  <v-main>
+    <v-container>
+      <p class="text-h3">Toolbox</p>
+      <div>
+        <p class="text-h5">Searchbar</p>
+      </div>
+      <div>
+        <p class="text-h5">Cards</p>
+      </div>
+      <div>
+        <p class="text-h5">Modal</p>
+      </div>
+      <div>
+        <p class="text-h5">ListGroup</p>
+      </div>
+      <div>
+        <p class="text-h5">ActionsMenu</p>
+      </div>
+      <div>
+        <p class="text-h5">Buttons</p>
+      </div>
+      <div>
+        <p class="text-h5">Photos</p>
+      </div>
+      <div>
+        <p class="text-h5">Icons</p>
+      </div>
+      <div>
+        <p class="text-h5">Textes</p>
+      </div>
+      <div>
+        <p class="text-h5">Form</p>
+        <div><p class="text-h6">TextInput</p></div>
+        <div><p class="text-h6">LocalisationInput</p></div>
+        <div><p class="text-h6">MediaInput</p></div>
+      </div>
+    </v-container>
+  </v-main>
+</template>
