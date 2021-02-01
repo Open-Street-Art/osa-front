f304a6912c98c67379b4d0f1b3168cd73cf8b92f
Arnaud Peralta <arnaud.peralta@gmail.com>
2021-02-01 14:23:57 +0100

    fix: vuetify installed

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,38 +4,38 @@ module.exports = {
 		node: true
   	},
   	extends: [
-		"plugin:vue/recommended",
+		'plugin:vue/recommended',
   	],
   	parserOptions: {
-		parser: "babel-eslint"
+		parser: 'babel-eslint'
   	},
 	rules: {
-		"no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-		"no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
-		"semi": ["error", "always"],
-		"indent": ["error", "tab"],
-		"quotes": ["error", "single", { "avoidEscape": true } ],
-		"vue/html-indent": ["error", "tab", {
-			"attribute": 1,
-			"baseIndent": 1,
-			"closeBracket": 0,
-			"alignAttributesVertically": true,
-			"ignores": []
+		'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+		'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+		'semi': ['error', 'always'],
+		'indent': ['error', 'tab'],
+		'quotes': ['error', 'single', { 'avoidEscape': true } ],
+		'vue/html-indent': ['error', 'tab', {
+			'attribute': 1,
+			'baseIndent': 1,
+			'closeBracket': 0,
+			'alignAttributesVertically': true,
+			'ignores': []
 		}],
-		"vue/max-attributes-per-line": ["error", {
-			"singleline": 1,
-			"multiline": {
-				"max": 1,
+		'vue/max-attributes-per-line': ['error', {
+			'singleline': 1,
+			'multiline': {
+				'max': 1,
 			},
 		}],
-		"vue/html-closing-bracket-newline": ["error", {
-			"singleline": "never",
-			"multiline": "never",
+		'vue/html-closing-bracket-newline': ['error', {
+			'singleline': 'never',
+			'multiline': 'never',
 		}],
-		"vue/multiline-html-element-content-newline": ["error", {
-			"ignoreWhenEmpty": true,
-			"ignores": ["pre", "textarea"],
-			"allowEmptyLines": false
+		'vue/multiline-html-element-content-newline': ['error', {
+			'ignoreWhenEmpty': true,
+			'ignores': ['pre', 'textarea'],
+			'allowEmptyLines': false
 		}]
   	}
 };
--- src/App.vue
+++ src/App.vue
@@ -13,3 +13,14 @@
 		</v-main>
 	</v-app>
 </template>
+
+<script>
+export default {
+	name: 'App',
+	data () {
+		return {
+			isProduction: process.env.NODE_ENV === 'production'
+		};
+	}
+};
+</script>
