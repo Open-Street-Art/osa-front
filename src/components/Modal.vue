f44e2b5fefc2fa8b142364ffa6ba711532d3fd9f
AudricBA <58440034+AudricBA@users.noreply.github.com>
2021-03-08 17:11:09 +0100

    Update Modal.vue

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -1,91 +1,52 @@
 <template>
-	<div
-		ref="blur"
-		class="blur"
-		@click="closeModal"
-		@mouseup="hideModal"
-		@mousemove="moveModal">
-		<div class="modal modal-mobile">
-			<svg
-				class="modal-arrow"
-				width="52"
-				height="16"
-				viewBox="0 0 52 16"
-				fill="none"
-				xmlns="http://www.w3.org/2000/svg"
-				@mousedown="selectArrow">
-				<path
-					d="M2.47214 0C0.361689 0 -0.309935 2.84503 1.57771 3.78885L25.1056 15.5528C25.6686 15.8343 26.3314 15.8343 26.8944 15.5528L50.4223 3.78885C52.3099 2.84503 51.6383 0 49.5279 0H2.47214Z"
-					fill="#FAFAFA" />
-			</svg>
-			<slot name="header" />
-			<slot name="content" />
-		</div>
-	</div>
+	<v-row justify="space-around">
+		<v-col cols="auto">
+			<v-dialog
+				transition="dialog-bottom-transition"
+				max-width="600">
+				<template v-slot:activator="{ on, attrs }">
+					<v-btn
+						color="primary"
+						v-bind="attrs"
+						v-on="on">
+						Connexion
+					</v-btn>
+				</template>
+				<template
+					v-slot:default>
+					<v-card>
+						<slot name="header" />
+						<v-card-text>
+							<div class="text-h2 pa-12">
+								<slot />
+							</div>
+						</v-card-text>
+					</v-card>
+				</template>
+			</v-dialog>
+		</v-col>
+	</v-row>
 </template>
-
 <script>
+import '../stories/Texts';
 export default {
 	name: 'Modal',
-	data: function() {
-		return {
-			arrowDown: false,
-		};
-	},
-	methods: {
-		closeModal: function(event) {
-
-		},
-		selectArrow: function(event) {
-			this.arrowDown = true;
-		},
-		moveModal: function(event) {
-		},
-		hideModal: function(event) {
-			if(this.arrowDown) {
-				this.arrowDown = false;
-			}
+	props: {
+		textModal: {
+			default: ' defaultLabel ',
+			type: String
 		}
 	}
 };
 </script>
-
 <style lang="scss">
 @import "../assets/styles/text.scss";
 
-.blur {
-  position: absolute;
-  top: 0;
-  left: 0;
-  z-index: 1000;
-
-  height: 100%;
-  width: 100%;
-
-  background-color: rgba(0,0,0,0.5);
-}
-
-.modal {
-  position: absolute;
-  background-color: $white-color;
-  overflow: hidden;
-}
-
-.modal-mobile {
-  top: 13px;
-  left: 13px;
-  border-radius: 20px 20px 0 0;
-  height: calc(100% - 13px);
-  width: calc(100% - 26px);
-
-}
-
-.modal-arrow {
-  position: absolute;
+.modal-text{
+ position: absolute;
   z-index: 100;
-  top: 13px;
+  top: 40px;
   left: 50%;
   transform: translateX(-50%);
 }
-
 </style>
