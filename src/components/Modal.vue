4d7ac4eef1f78dd12c7fa74bfd2862fce3a3ad7a
Nouha-06 <nouhaila.mellouki@gmail.com>
2021-03-11 22:07:35 +0100

    fix:fixing Modal

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -1,90 +1,62 @@
 <template>
-	<v-row justify="space-around">
-		<v-col cols="auto">
-			<v-dialog
-				transition="dialog-bottom-transition"
-				max-width="600">
-				<template v-slot:activator="{ on, attrs }">
-					<v-btn
-						color="primary"
-						v-bind="attrs"
-						v-on="on">
-						From the button
-					</v-btn>
-				</template>
-				<template
-					v-slot:default>
-					<v-card>
-						<slot name="header" />
-						<v-card-text>
-							<div class="text-h2 pa-12">
-								<slot />
-							</div>
-						</v-card-text>
-					</v-card>
-				</template>
-			</v-dialog>
-		</v-col>
-	</v-row>
+	<v-bottom-sheet
+		transition="dialog-bottom-transition"
+		max-width="98%"
+		scrollable
+		:value="value">
+		<v-card
+			class="content rounded-t-xl"
+			transition="dialog-bottom-transition">
+			<svg
+				class="modal-arrow"
+				width="52"
+				height="16"
+				viewBox="0 0 52 16"
+				fill="none"
+				xmlns="http://www.w3.org/2000/svg"
+				@click="$emit('close')">
+				<path
+					d="M2.47214 0C0.361689 0 -0.309935 2.84503 1.57771 3.78885L25.1056 15.5528C25.6686 15.8343 26.3314 15.8343 26.8944 15.5528L50.4223 3.78885C52.3099 2.84503 51.6383 0 49.5279 0H2.47214Z"
+					fill="#FAFAFA" />
+			</svg>
+			<slot />
+		</v-card>
+	</v-bottom-sheet>
 </template>
 <script>
-import '../stories/Texts';
 export default {
 	name: 'Modal',
+	model: {
+		prop: 'value',
+		event: 'update'
+	},
 	props: {
-		textModal: {
-			default: ' defaultLabel ',
-			type: String
+		value: {
+			type: Boolean,
+			default: false
 		}
 	}
 };
 </script>
-<style lang="scss">
-@import "../assets/styles/text.scss";
 
-.modal-text{
- position: absolute;
-  z-index: 100;
-  top: 40px;
-  left: 50%;
-  transform: translateX(-50%);
-}
-</style><style lang="scss">
-@import "../assets/styles/text.scss";
-
-.blur {
-  position: absolute;
-  top: 0;
-  left: 0;
-  z-index: 1000;
-
-  height: 100%;
-  width: 100%;
-
-  background-color: rgba(0,0,0,0.5);
-}
-
-.modal {
-  position: absolute;
-  background-color: $white-color;
-  overflow: hidden;
-}
-
-.modal-mobile {
-  top: 13px;
-  left: 13px;
-  border-radius: 20px 20px 0 0;
-  height: calc(100% - 13px);
-  width: calc(100% - 26px);
-
+<style scoped lang="scss">
+@import "../assets/styles/_variables.scss";
+.content {
+	margin-top: 0px;
+    background-color: $white-color;
+    width: 90%;
+    min-height: 200vh;
+    overflow: hidden;
+	margin-top: -9vh;
 }
 
 .modal-arrow {
-  position: absolute;
-  z-index: 100;
-  top: 13px;
-  left: 50%;
-  transform: translateX(-50%);
+    position: absolute;
+    z-index: 100;
+    top: 13px;
+    left: 50%;
+    transform: translateX(-50%);
+    border-top-left-radius: 0px !important;
+    border-top-right-radius: 0px !important;
 }
-
 </style>
