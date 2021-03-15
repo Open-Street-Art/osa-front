a2078638660cb592fd07c654dc8f1cfa75484e9d
Goffayoh <yohann.goffart@etu.univ-rouen.fr>
2021-03-15 18:08:58 +0100

    feat: locale changer

--- src/plugins/i18n.js
+++ src/plugins/i18n.js
@@ -1,5 +1,6 @@
 import Vue from 'vue';
 import VueI18n from 'vue-i18n';
+import store  from '../store/index.js';
 
 Vue.use(VueI18n);
 
@@ -17,7 +18,7 @@ function loadLocaleMessages() {
 }
 
 export default new VueI18n({
-	locale: process.env.VUE_APP_I18N_LOCALE || 'fr',
+	locale: store.getters.getAppLanguage,
 	fallbackLocale: process.env.VUE_APP_I18N_FALLBACK_LOCALE || 'fr',
 	messages: loadLocaleMessages(),
 });
--- src/store/index.js
+++ src/store/index.js
@@ -4,8 +4,19 @@ import Vuex from 'vuex';
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-	state: {},
-	mutations: {},
+	state: {
+		appLanguage: localStorage.getItem('appLanguage') || process.env.VUE_APP_I18N_LOCALE || 'fr'
+	},
+	getters: {
+		getAppLanguage: (state) => state.appLanguage
+	},
+	mutations: {
+		setAppLanguage(state, language) {
+			state.appLanguage = language;
+			localStorage.setItem('appLanguage', language);
+			console.log('test : '+localStorage.getItem('appLanguage'));
+		}
+	},
 	actions: {},
 	modules: {}
 });
